5dadb257f14d2b33a7796301aa9c747b65ea623e
Antonio Cisneros <acip0903@gmail.com>
2025-05-24 16:16:54 +1000

    Yelp styled

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,5 +1,5 @@
 class ReviewsController < ApplicationController
-  before_action :set_restaurant, only: %i[new create]
+  before_action :set_restaurant, only: %i[create]
 
   def create
     @review = Review.new(review_params)
